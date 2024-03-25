01f87ae91ab7b6fe6dd1e1deb3807fe2ee108af6
mtiann3 <120132854+mtiann3@users.noreply.github.com>
2024-03-24 21:15:23 -0500

    Initial Commit

---
+++ AthleteIQ/AthleteIQApp.swift
@@ -0,0 +1,17 @@
+//
+//  AthleteIQApp.swift
+//  AthleteIQ
+//
+//  Created by Mike Iannotti on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct AthleteIQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AthleteIQ/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AthleteIQ
+//
+//  Created by Mike Iannotti on 3/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
