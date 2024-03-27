c6faa8b05c398b9e228b4152ed81a13ee0a4d1b4
mtiann3 <120132854+mtiann3@users.noreply.github.com>
2024-03-26 19:47:44 -0500

    Added graph main component and UI fixes

---
+++ AthleteIQ/Components/LineChart.swift
@@ -0,0 +1,18 @@
+//
+//  LineChart.swift
+//  AthleteIQ
+//
+//  Created by Mike Iannotti on 3/26/24.
+//
+
+import SwiftUI
+
+struct LineChart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LineChart()
+}
