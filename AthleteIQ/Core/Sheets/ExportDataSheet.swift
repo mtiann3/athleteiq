d683ac636d6c78d1fae60566b9a2abeb512df386
mtiann3 <120132854+mtiann3@users.noreply.github.com>
2024-03-28 18:51:02 -0500

    Generate csv function added back accidentally removed earlier

--- AthleteIQ/Core/Sheets/ExportDataSheet.swift
+++ AthleteIQ/Core/Sheets/ExportDataSheet.swift
@@ -1,18 +1,53 @@
-//
-//  ExportDataSheet.swift
-//  AthleteIQ
-//
-//  Created by Mike Iannotti on 3/27/24.
-//
-
 import SwiftUI
+import SwiftData
+import Foundation
 
 struct ExportDataSheet: View {
+    @Environment(\.modelContext) var context
+    @Query(sort: \Exercise.date)
+    var exercises: [Exercise]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Exporting...")
+            .onAppear {
+                exportCSV()
+            }
+    }
+
+    func exportCSV() {
+        let fileName = "exercise_data.csv"
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent(fileName)
+        
+        var csvText = "Name,Date,Weight,Repetitions,Sets\n"
+        for exercise in exercises {
+            let name = exercise.name
+            let date = formatDate(exercise.date)
+            let weight = "\(exercise.weight)"
+            let repetitions = "\(exercise.repetitions)"
+            let sets = "\(exercise.sets)"
+            let newLine = "\(name),\(date),\(weight),\(repetitions),\(sets)\n"
+            csvText.append(newLine)
+        }
+        
+        do {
+            try csvText.write(to: path, atomically: true, encoding: .utf8)
+            print("CSV file created at: \(path)")
+            // You might want to add an alert or other UI feedback to inform the user that the export is successful
+        } catch {
+            print("Failed to create CSV file: \(error.localizedDescription)")
+            // You might want to add an alert or other UI feedback to inform the user about the failure
+        }
+    }
+    
+    func formatDate(_ date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return dateFormatter.string(from: date)
     }
 }
 
-#Preview {
-    ExportDataSheet()
+struct ExportDataSheet_Previews: PreviewProvider {
+    static var previews: some View {
+        ExportDataSheet()
+    }
 }
--- AthleteIQ/Core/Tabs/ProfileTabView.swift
+++ AthleteIQ/Core/Tabs/ProfileTabView.swift
@@ -1,3 +1,10 @@
+//
+//  ProfileTabView.swift
+//  AthleteIQ
+//
+//  Created by Mike Iannotti on 3/25/24.
+//
+
 import SwiftUI
 import SwiftData
 
@@ -9,70 +16,76 @@ struct ProfileTabView: View {
     var exercises: [Exercise]
     
     var totalSets: Int {
-        exercises.reduce(0) { $0 + $1.sets }
-    }
-    
-    var totalReps: Int {
-        exercises.reduce(0) { $0 + ($1.sets * $1.repetitions) }
-    }
+           exercises.reduce(0) { $0 + $1.sets }
+       }
+       
+       var totalReps: Int {
+           exercises.reduce(0) { $0 + ($1.sets * $1.repetitions) }
+       }
     
     var body: some View {
-        NavigationView {
-            List {
-                Section(header:
-                    VStack(alignment: .leading, spacing: 4) {
-                        Text("AthleteIQ")
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
-                        Text("Your Personal Fitness Assistant")
-                            .font(.subheadline)
-                            .foregroundColor(.gray)
-                    }
-                    .padding(.vertical, 12)
-                ) {
-                    // Placeholder for any additional header content
-                }
-                
-                Section("General") {
-                    VStack {
-                        InfoRow(title: "Total Exercises Entered:", value: "\(exercises.count)")
-                        InfoRow(title: "Total Sets Performed:", value: "\(totalSets)")
-                        InfoRow(title: "Total Repetitions Performed:", value: "\(totalReps)")
-                    }
-                }
-                
-                Section("Export Data") {
-                    Button(action: {
-                        isShowingExportDataSheet.toggle()
-                    }) {
-                        Label("Export CSV", systemImage: "list.bullet.rectangle.portrait")
-                    }
-                }
-                
-                Section("Help") {
-                    Button(action: {
-                        isShowingItemSheet.toggle()
-                    }) {
-                        HStack{
-                            Text("App Instructions")
-                            Spacer()
-                            Image(systemName: "arrow.right")
+        List{
+            Section(header:
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text("AthleteIQ")
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                            Text("Your Personal Fitness Assistant")
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
                         }
+                        .padding(.vertical, 12)
+            ) {
+            }
+
+            Section("General") {
+                               VStack {
+                                   InfoRow(title: "Total Exercises Entered:", value: "\(exercises.count)")
+                                   InfoRow(title: "Total Sets Performed:", value: "\(totalSets)")
+                                   InfoRow(title: "Total Repetitions Performed:", value: "\(totalReps)")
+                               }
+                           }
+
+            Section("Export Data") {
+                VStack {
+                            
+                    ShareLink(item:generateCSV(exercises: exercises)!) {
+                                Label("Export CSV", systemImage: "list.bullet.rectangle.portrait")
+                            }
+                            
+                        }
+                        .padding()
+            }
+            
+            
+            Section("Help"){
+                Button(action: {
+//              Open helpsheetview
+                    isShowingItemSheet = true
+                }) {
+                    HStack{
+                        Text("App Instructions")
+                        Spacer()
+                        Image(systemName: "arrow.right")
                     }
                 }
-            }
-            .listStyle(InsetGroupedListStyle())
             
-            .sheet(isPresented: $isShowingItemSheet) {
-                HelpSheet()
+               
             }
-            .sheet(isPresented: $isShowingExportDataSheet) {
-                ExportDataSheet()
-            }
+                
+            
         }
+        .sheet(isPresented: $isShowingItemSheet) {
+            HelpSheet()
+        }
+        .sheet(isPresented: $isShowingExportDataSheet) {
+            ExportDataSheet()
+        }
+        
+        
     }
+    
 }
-
 struct InfoRow: View {
     let title: String
     let value: String
@@ -92,8 +105,45 @@ struct InfoRow: View {
     }
 }
 
-struct ProfileTabView_Previews: PreviewProvider {
-    static var previews: some View {
-        ProfileTabView()
+func generateCSV(exercises: [Exercise]) -> URL? {
+    var fileURL: URL?
+    
+    // heading of CSV file.
+    let heading = "Name, Date, Weight, Repetitions, Sets\n"
+    
+    // file rows
+    let rows = exercises.map { "\(cleanCSVString($0.name)), \($0.date), \($0.weight), \($0.repetitions), \($0.sets)" }
+    
+    // rows to string data
+    let stringData = heading + rows.joined(separator: "\n")
+    
+    do {
+        let path = try FileManager.default.url(for: .documentDirectory,
+                                               in: .userDomainMask,
+                                               appropriateFor: nil,
+                                               create: false)
+        
+        fileURL = path.appendingPathComponent("Exercise-Data.csv")
+        
+        // append string data to file
+        try stringData.write(to: fileURL!, atomically: true, encoding: .utf8)
+        print("CSV file created at: \(fileURL!.path)")
+        
+    } catch {
+        print("Error generating CSV file: \(error)")
     }
+    
+    return fileURL
+}
+
+func cleanCSVString(_ string: String) -> String {
+    var cleanedString = string.replacingOccurrences(of: ",", with: "")
+    cleanedString = cleanedString.replacingOccurrences(of: "\n", with: " ")
+    return cleanedString
+}
+
+
+
+#Preview {
+    ProfileTabView()
 }
