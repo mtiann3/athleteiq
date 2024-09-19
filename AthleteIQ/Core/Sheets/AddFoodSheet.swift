8de668ea2c492357888c8afb6d7e4c98e76b9437
mtiann3 <120132854+mtiann3@users.noreply.github.com>
2024-09-19 18:48:05 -0500

    commit

---
+++ AthleteIQ/Core/Sheets/AddFoodSheet.swift
@@ -0,0 +1,18 @@
+//
+//  AddFoodSheet.swift
+//  AthleteIQ
+//
+//  Created by Mike Iannotti on 9/19/24.
+//
+
+import SwiftUI
+
+struct AddFoodSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddFoodSheet()
+}
---
+++ AthleteIQ/Core/Sheets/AddSleepSheet.swift
@@ -0,0 +1,18 @@
+//
+//  AddSleepSheet.swift
+//  AthleteIQ
+//
+//  Created by Mike Iannotti on 9/19/24.
+//
+
+import SwiftUI
+
+struct AddSleepSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddSleepSheet()
+}
